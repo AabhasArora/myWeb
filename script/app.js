d37fe0ba1ee5ccd44131cf171fb1254487f07af2
Aabhas Arora <aabhas.arora@aspiringminds.in>
2015-09-10 13:08:17 +0530

    Test data

---
+++ script/app.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Aabhas Arora on 10-09-2015.
+ */
+;(function ($) {
+    'use strict';
+    var $body = $('html, body'),
+        $main = $('#main'),
+        options = {
+            prefetch: true,
+            pageCacheSize: 4,
+            onStart: {
+                duration: 250,
+                render: function (url, $container) {
+                    $body.animate({
+                        scrollTop: 0
+                    });
+                    $main.addClass('is-exiting');
+                    smoothState.restartCSSAnimations();
+                }
+            },
+            onEnd: {
+                duration: 0,
+                render: function (url, $container, $content) {
+                    $main.removeClass('is-exiting');
+                    $main.html($content);
+                    $body.css('cursor', 'auto');
+                    $body.find('a').css('cursor', 'auto');
+                }
+            }
+        },
+        smoothState = $main.smoothState(options).data('smoothState');
+})(jQuery);
